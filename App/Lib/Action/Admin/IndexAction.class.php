e5e431109eb8a88e06490e9185d7ef7ca77592e6
Daniel Ma <danielma@bubargain.com>
2012-12-11 23:37:32 +0800

    canceled the UCcenter component

--- App/Lib/Action/Admin/IndexAction.class.php
+++ App/Lib/Action/Admin/IndexAction.class.php
@@ -13,10 +13,10 @@ class IndexAction extends CommonAction {
         if ($_SESSION['__account__'])
         {
           header("Content-Type:text/html; charset=utf-8");
-          import('@.ORG.Uc'); //uhome同步登陆
-          $Uc = new Uc();
-          $data = $Uc->synclogin($_SESSION['__account__'],strrev($_SESSION['__password__']));
-          unset($_SESSION['__account__'],$_SESSION['__password__']);
+          //import('@.ORG.Uc'); //uhome同步登陆
+          //$Uc = new Uc();
+          //$data = $Uc->synclogin($_SESSION['__account__'],strrev($_SESSION['__password__']));
+          //unset($_SESSION['__account__'],$_SESSION['__password__']);
         }
 		C ( 'SHOW_RUN_TIME', false ); // 运行时间显示
 		C ( 'SHOW_PAGE_TRACE', false );
