dd7290174f94fa68db0d828820d9574027ddb722
Daniel Ma <danielma@bubargain.com>
2013-01-04 22:24:01 +0800

    Translation

--- editor/demo.php
+++ editor/demo.php
@@ -28,13 +28,13 @@ $ticket = $_SGLOBAL['db']->fetch_array($query);
 		</script>
 	</head>
 	<body>
-		<h3>优惠券编辑器——<?php echo $_REQUEST['name'];?></h3>
+		<h3>Coupon Editer——<?php echo $_REQUEST['name'];?></h3>
 		<iframe name="saveIframe" id="saveIframe" style="display:none;"></iframe>
 		<form id="editorForm" name="editorForm" method="post" action="php/save2.php">
         <input type="hidden" id="tid" name="tid" value="<?php echo $_REQUEST['id'];?>" />
 			<input type="hidden" id="htmlData" name="htmlData" value="" />
 			<textarea id="content" name="content" style="width:929px;height:<?php echo $_SCONFIG['gif_height']; ?>px;visibility:hidden;"><?php echo $ticket['html']; ?></textarea>
-            <input type="submit" value="保存" />
+            <input type="submit" value="Save" />
 		</form>
 	</body>
 </html>
